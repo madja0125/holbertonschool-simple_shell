8d4f576c351b1a66a9be93fd6e7b3a391bafeefb
madja0125 <dejesusmiguel788@gmail.com>
2023-12-20 22:24:35 +0000

    adding main code

---
+++ sshell.h
@@ -0,0 +1,30 @@
+#ifndef SSHELL_H
+#define SSHELL_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <dirent.h>
+#include <ctype.h>
+
+/* unused */
+int main(int ac __attribute__((unused)), char **av);
+
+/* Shell User line functions */
+void prompt(void);
+void execute(char *line);
+char **parse_arguments(char *line);
+void free_args(char **args);
+int check_for_builtins(char *command, char **args);
+int is_empty(const char *str);
+char *search_executable_in_path(const char *executable);
+void prompt(void);
+
+#endif
