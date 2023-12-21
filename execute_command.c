4591bc85405297ea073a04795fc1c28f0caf7792
madja0125 <dejesusmiguel788@gmail.com>
2023-12-21 22:59:06 +0000

    adding file that excutes user lines

---
+++ execute_command.c
@@ -0,0 +1,32 @@
+#include "sshell.h"
+
+/**
+ * execute - function that takes a an user input en executes it
+ * @line: user input
+ * Return: void
+ */
+
+void execute(char *line)
+{
+	char **args;
+
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		args = parse_arguments(line);
+		execvp(args[0], args);
+		perror(args[0]);
+		free_args(args);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+	}
+}
