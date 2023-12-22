9b52b261b698ef9d3f5f6e8bc481ce756f5139da
xov121 <x.o.v121@gmail.com>
2023-12-22 09:20:24 -0400

    Implemented exit.

--- main.c
+++ main.c
@@ -10,15 +10,20 @@ int main(void)
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	
+
 	/* Check if in interactive mode */
 	if (isatty(STDIN_FILENO))
 	prompt();
 
 	while ((read = getline(&line, &len, stdin)) != -1)
 	{
-		if (read > 0 && line[read - 1] == 0)
+		if (read > 0 && line[read - 1] == '\n')
 		line[read - 1] = '\0';
+
+		if (strcmp(line, "exit") == 0)
+		{
+			break;
+		}
 
 		if (is_empty(line))
 		{
